a5045000d1e299f61952608bb8273212a1f7f11d
Serkan Çay <sserkancay@gmail.com>
2019-06-03 22:03:52 +0300

    Create StyleFragment

---
+++ app/src/main/java/com/serkancay/beautext/ui/style/StyleFragment.kt
@@ -0,0 +1,17 @@
+package com.serkancay.beautext.ui.style
+
+import com.serkancay.beautext.R.layout
+import com.serkancay.beautext.ui.base.BaseFragment
+
+/**
+ *
+ * Created by S.Serkan Cay on 3.06.2019
+ *
+ */
+
+class StyleFragment : BaseFragment() {
+
+    override fun getLayoutId(): Int {
+        return layout.fragment_style
+    }
+}
